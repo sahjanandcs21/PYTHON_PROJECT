34edc9e17e7ec221817e0a2c94bfaa3f14cfcb4a
sahjanandcs21 <101086594+sahjanandcs21@users.noreply.github.com>
2022-05-21 22:39:38 +0530

    Create iterate_over_dictionaries.py

---
+++ Dictionary/iterate_over_dictionaries.py
@@ -0,0 +1,10 @@
+dict_1 = {'Name': 'Sam', 'Age': 26, 'City': 'Toronto', 'Degree': 'Masters'}
+
+# Method 1
+print(dict_1.items())
+print('')
+
+# Method 2
+print('Dictionary Key-Value pairs are:')
+for i in dict_1.items():
+    print(i)
