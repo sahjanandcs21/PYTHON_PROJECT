c8741e2c130c2cc248f1a0c62bf9c2055bfa8e99
sahjanandcs21 <101086594+sahjanandcs21@users.noreply.github.com>
2022-05-21 23:54:22 +0530

    Create multiplication of two given matrixes.py

---
+++ Numpy/multiplication of two given matrixes.py
@@ -0,0 +1,10 @@
+import numpy as np
+p = [[1, 0], [0, 1]]
+q = [[1, 2], [3, 4]]
+print("original matrix:")
+print(p)
+print(q)
+result1 = np.dot(p, q)
+print("Result of the said matrix multiplication:")
+print(result1)
+print(result2)
